470e7987f8db104cc8365e230a83d7475c313a21
CrepeGoat <awqatty.b@gmail.com>
2020-11-13 11:35:05 -0500

    solved traits2.rs

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,10 +10,16 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
+}
+
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut result = self.clone();
+        result.push("Bar".to_string());
+        result
+    }
 }
 
 //TODO: Add your code here
